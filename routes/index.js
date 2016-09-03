82d9caa10ad925a0b62f06ebe24023f1bb15be80
DeveloperDemetri <developerdemetri@gmail.com>
2016-09-03 01:44:43 -0700

    replace bcrypt package due to issues with windows. The new package is pure js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ let app = require('../app');
 let express = require('express');
 let db_pool = require('../bin/db_pool');
 let aes_tool = require('../bin/aes_tool');
-let bcrypt = require('bcrypt');
+let bcrypt = require('bcrypt-nodejs');
 let redis_tool = require('../bin/redis_tool');
 let session_tool = require('../bin/session_tool');
 
