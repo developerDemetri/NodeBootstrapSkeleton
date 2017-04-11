182da82dca5ce6078c2418256140aa0a0654fb8b
Demetri <developerDemetri@users.noreply.github.com>
2017-04-10 20:32:28 -0700

    Updating validation tool

--- bin/validator_tool.js
+++ bin/validator_tool.js
@@ -3,11 +3,17 @@
 let validator_tool = {};
 
 validator_tool.checkInput = function(input, type, regex) {
-  if (input && (typeof input) === type) {
-    if (type === 'string' && !regex.test(input)) {
-      return false;
+  if (!(input === null || input === undefined)) {
+    if (type === 'string' && regex) {
+      if (typeof(input) === 'string' && regex.test(input)) {
+        return true;
+      }
     }
-    return true;
+    else if (type === 'number') {
+      if (typeof(input) === 'number' || !(isNaN(input))) {
+        return true;
+      }
+    }
   }
   return false;
 }
